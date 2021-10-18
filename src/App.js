5adc7ddfce1ca82b9f7d67f41865d0cf8c9b422d
Abhishek <980vermarajput@gmail.com>
2021-10-18 20:22:01 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Board from "./components/Board";
+import "./style/root.scss"
 
 function app(){
   return(
-    <>
+    <div className="app">
       <h1>TIK TAK TOE</h1>
       <Board/>
-    </>
+    </div>
   )
 };
 export default app;
