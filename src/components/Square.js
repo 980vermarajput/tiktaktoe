f911f6ed608a5fb3f11f6be07ebf32a53fc601d1
Abhishek <980vermarajput@gmail.com>
2021-10-21 20:12:19 +0530

    states used for X O

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Square =({value}) => {
+const Square =({value,onclick}) => {
     return (
         
-    <button type="button" className="square" >{value}</button>
+    <button type="button" className="square" onClick={onclick} >{value}</button>
         
     )
 }
